70af2b945ae993cd2d850bf420013d0beb3fc73b
surya sama <suryasama4@gmail.com>
2021-07-19 17:21:53 -0400

    Initial Commit

---
+++ gitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitHub
+//
+//  Created by Surya Sama on 2021-07-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitHub/gitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubApp.swift
+//  gitHub
+//
+//  Created by Surya Sama on 2021-07-19.
+//
+
+import SwiftUI
+
+@main
+struct gitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
